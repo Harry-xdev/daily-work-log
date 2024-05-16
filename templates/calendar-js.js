1ed091e25b4af94bf2e5a0aaac8b84ebb8218ed8
Harry-xdev <harry.xdev@gmail.com>
2024-05-16 19:12:36 +0700

    calendar updated

--- templates/calendar-js.js
+++ templates/calendar-js.js
@@ -3,10 +3,12 @@ let currentDate = new Date();
 // Extract the individual components of the date
 let year = currentDate.getFullYear();
 let monthGlobal = currentDate.getMonth() + 1; // Months are zero-based, so we add 1
-let day = currentDate.getDate();
+let date = currentDate.getDate();
+let weekday = currentDate.getDay();
 if (monthGlobal < 10) {
     monthGlobal = '0' + (monthGlobal - 0);
 };
+
 
 document.addEventListener("DOMContentLoaded", function () {
     let myParagraph = document.getElementById("month");
@@ -24,6 +26,30 @@ buttons.forEach(button => {
             console.log(data);
             // Get the current date
             // let currentDate = new Date();
+            let x = 0; 
+            if (1 <= date && date <= 7) {
+                x = 1;
+            } else if (8 <= date && date <= 14) {
+                x = 2;
+            } else if (15 <= date && date <= 21) {
+                x = 3;
+            } else if (22 <= date && date <= 28) {
+                x = 4;
+            } else if (29 <= date && date <= 31) {
+                x = 5;
+            };
+
+            let condition = (7 * x - (7 - weekday)) - (date - 1);
+            if (condition < 1) {
+                x = x + 1;
+            }
+
+            firstDateOfMonth = (7 * x - (7 - weekday)) - (date - 1);
+
+            console.log(x);
+            console.log(date);
+            console.log(`First day of month: ${firstDateOfMonth}`);
+
 
             
             let staffName;
@@ -54,85 +80,65 @@ buttons.forEach(button => {
             let trimmedDates = dates.map(date => date.replace(/^0*(\d+)/, '$1'));
             let dateNumber = dates.map(date => +date);
             let overTime = filteredMonth.map(data => data.leaving_time);
-    
             console.log(trimmedDates);
     
     
-            const row = [1, 2, 3, 4, 5, 6, 7];
+            const row = ['MON','TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
             const divContainer0 = document.getElementById('day-container0');
     
-            row.map((row) => {
-                const div = document.createElement('div');
-                div.textContent = row;
-                div.classList.add('weekday');
-                divContainer0.appendChild(div);
-            });
-    
-            let calendarData = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
-    
+            // row.map((row) => {
+            //     const div = document.createElement('div');
+            //     div.textContent = row;
+            //     div.classList.add('weekday');
+            //     divContainer0.appendChild(div);
+            // });
+
+
+            let calendarEmptySlot = new Array(31 + (firstDateOfMonth-1)).fill('');
+
             for (let i = 0; i < dateNumber.length; i++) {
                 let idx = dateNumber[i];
-                if (idx >= 0 && idx < calendarData.length) {
-                    calendarData[idx - 1] = overTime[i];
+                if (idx >= 0 && idx < calendarEmptySlot.length) {
+                    calendarEmptySlot[idx + firstDateOfMonth - (firstDateOfMonth - 1)] = overTime[i];
+
                 };
             };
-    
-    
+
+        
             const divContainer1 = document.getElementById('day-container1');
-            calendarData.map((item, index) => {
+            calendarEmptySlot.map((item, index) => {
                 const div = document.createElement('div');
                 div.textContent = item;
                 div.classList.add('day');
                 divContainer1.appendChild(div);
-    
-                if (index === 6) {
-                    const row = [8, 9, 10, 11, 12, 13, 14];
-                    row.map((row) => {
-                        const div = document.createElement('div');
-                        div.textContent = row;
-                        div.classList.add('weekday');
-                        divContainer1.appendChild(div);
-    
-                    });
-                };
-                if (index === 13) {
-                    const row = [15, 16, 17, 18, 19, 20, 21];
-                    row.map((row) => {
-                        const div = document.createElement('div');
-                        div.textContent = row;
-                        div.classList.add('weekday');
-                        divContainer1.appendChild(div);
-    
-                    });
-                };
-                if (index === 20) {
-                    const row = [22, 23, 24, 25, 26, 27, 28];
-                    row.map((row) => {
-                        const div = document.createElement('div');
-                        div.textContent = row;
-                        div.classList.add('weekday');
-                        divContainer1.appendChild(div);
-    
-                    });
-                };
-    
-                if (index === 27) {
-                    const row = ['29', '30', '31', '', '', '', ''];
-                    row.map((row) => {
-                        const div = document.createElement('div');
-                        div.textContent = row;
-                        div.classList.add('weekday');
-                        divContainer1.appendChild(div);
-    
-                    });
-                };
-    
-    
-    
-    
+
+                // if (index === 6) {
+                //     const row = [8, 9, 10, 11, 12, 13, 14];
+                //     row.map((row) => {
+                //         const div = document.createElement('div');
+                //         div.textContent = row;
+                //         div.classList.add('weekday');
+                //         divContainer1.appendChild(div);
     
             });
-    
+
+            const dateDivContainers = document.querySelectorAll('.day');
+            let startIndex = firstDateOfMonth - 1; // Index of the 3rd div (0-based)
+            let endIndex = 35; // Index of the 8th div (inclusive)
+            dateDivContainers.forEach((container, index) => {
+                const dateDiv = document.createElement('div');
+
+                if (index >= startIndex && index < endIndex) {
+                    dateDiv.textContent = index - startIndex + 1;
+                } else {
+                    dateDiv.textContent = '';
+                }
+                dateDiv.classList.add('dateDisplay');
+                container.appendChild(dateDiv);
+            });
+
+
+
         })
         .catch(error => {
             console.log('Error:', error);
--- templates/pre-calendar-js.js
+++ templates/pre-calendar-js.js
@@ -3,10 +3,11 @@ let currentDate = new Date();
 // Extract the individual components of the date
 let year = currentDate.getFullYear();
 let monthGlobal = currentDate.getMonth() + 1; // Months are zero-based, so we add 1
-let day = currentDate.getDate();
+let date = currentDate.getDate();
 if (monthGlobal < 10) {
     monthGlobal = '0' + (monthGlobal - 1);
 };
+
 
 document.addEventListener("DOMContentLoaded", function () {
     let myParagraph = document.getElementById("month");
@@ -19,12 +20,18 @@ buttons.forEach(button => {
         fetch('http://192.168.0.115:5000/api/data')
         .then(response => response.json())
         .then(data => {
-    
             // Populate the list with data
             console.log(data);
             // Get the current date
             // let currentDate = new Date();
+            let monthGlobal2 = currentDate.getMonth() - 1;
 
+            console.log(`month ${monthGlobal2}`);
+            let firstDateOfMonth;
+            const firstDayOfMonth = new Date(year, monthGlobal2, 1);
+            const weekdays = [1, 2, 3, 4, 5, 6, 7];
+            firstDateOfMonth = weekdays[firstDayOfMonth.getDay() - 1];
+            console.log(firstDateOfMonth);
             
             let staffName;
             text = button.getAttribute('title');
@@ -54,85 +61,67 @@ buttons.forEach(button => {
             let trimmedDates = dates.map(date => date.replace(/^0*(\d+)/, '$1'));
             let dateNumber = dates.map(date => +date);
             let overTime = filteredMonth.map(data => data.leaving_time);
+            console.log(dateNumber);
+            console.log(overTime);
     
-            console.log(trimmedDates);
     
-    
-            const row = [1, 2, 3, 4, 5, 6, 7];
+            const row = ['MON','TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
             const divContainer0 = document.getElementById('day-container0');
     
-            row.map((row) => {
-                const div = document.createElement('div');
-                div.textContent = row;
-                div.classList.add('weekday');
-                divContainer0.appendChild(div);
-            });
-    
-            let calendarData = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
-    
+            // row.map((row) => {
+            //     const div = document.createElement('div');
+            //     div.textContent = row;
+            //     div.classList.add('weekday');
+            //     divContainer0.appendChild(div);
+            // });
+
+
+            let calendarEmptySlot = new Array(31 + (firstDateOfMonth-1)).fill('');
+
             for (let i = 0; i < dateNumber.length; i++) {
                 let idx = dateNumber[i];
-                if (idx >= 0 && idx < calendarData.length) {
-                    calendarData[idx - 1] = overTime[i];
+                if (idx >= 0 && idx < calendarEmptySlot.length) {
+                
+                    calendarEmptySlot[idx - 1 + (firstDateOfMonth - (firstDateOfMonth - 1) - 1)] = overTime[i];
+                    // console.log(idx + firstDateOfMonth - firstDateOfMonth - 1);
                 };
             };
-    
-    
+
+        
             const divContainer1 = document.getElementById('day-container1');
-            calendarData.map((item, index) => {
+            calendarEmptySlot.map((item, index) => {
                 const div = document.createElement('div');
                 div.textContent = item;
                 div.classList.add('day');
                 divContainer1.appendChild(div);
-    
-                if (index === 6) {
-                    const row = [8, 9, 10, 11, 12, 13, 14];
-                    row.map((row) => {
-                        const div = document.createElement('div');
-                        div.textContent = row;
-                        div.classList.add('weekday');
-                        divContainer1.appendChild(div);
-    
-                    });
-                };
-                if (index === 13) {
-                    const row = [15, 16, 17, 18, 19, 20, 21];
-                    row.map((row) => {
-                        const div = document.createElement('div');
-                        div.textContent = row;
-                        div.classList.add('weekday');
-                        divContainer1.appendChild(div);
-    
-                    });
-                };
-                if (index === 20) {
-                    const row = [22, 23, 24, 25, 26, 27, 28];
-                    row.map((row) => {
-                        const div = document.createElement('div');
-                        div.textContent = row;
-                        div.classList.add('weekday');
-                        divContainer1.appendChild(div);
-    
-                    });
-                };
-    
-                if (index === 27) {
-                    const row = ['29', '30', '31', '', '', '', ''];
-                    row.map((row) => {
-                        const div = document.createElement('div');
-                        div.textContent = row;
-                        div.classList.add('weekday');
-                        divContainer1.appendChild(div);
-    
-                    });
-                };
-    
-    
-    
-    
+
+                // if (index === 6) {
+                //     const row = [8, 9, 10, 11, 12, 13, 14];
+                //     row.map((row) => {
+                //         const div = document.createElement('div');
+                //         div.textContent = row;
+                //         div.classList.add('weekday');
+                //         divContainer1.appendChild(div);
     
             });
-    
+
+            const dateDivContainers = document.querySelectorAll('.day');
+            let startIndex = firstDateOfMonth - 1; // Month are zero-base
+            let endIndex = 35; // Index of the 8th div (inclusive)
+            dateDivContainers.forEach((container, index) => {
+                const dateDiv = document.createElement('div');
+
+                if (index >= startIndex && index < endIndex) {
+                    dateDiv.textContent = index - startIndex + 1;
+                } else {
+                    dateDiv.textContent = '';
+                }
+                dateDiv.classList.add('dateDisplay');
+                container.appendChild(dateDiv);
+            });
+
+
+
         })
         .catch(error => {
             console.log('Error:', error);
